f1b4f1a4ce73d480553ac40ba0b86ced906f80ce
Rezmodeus <rezmodeus@yahoo.com>
2017-08-08 00:08:51 +0200

    Wip setting up new node

--- src/lib/Node.js
+++ src/lib/Node.js
@@ -5,79 +5,105 @@ var Draggable = require('react-draggable');
 
 import NodeInputList from './NodeInputList';
 import NodeOuputList from './NodeOutputList';
+import QuestText from './QuestText.react';
 
 class Node extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: false
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: false
+		}
 
-  handleDragStart(event, ui) {
-    this.props.onNodeStart(this.props.nid, ui);
-  }
+		this.onStartConnector = this.onStartConnector.bind(this);
+		this.onCompleteConnector = this.onCompleteConnector.bind(this);
+		this.handleDragStart = this.handleDragStart.bind(this);
+		this.handleDragStop = this.handleDragStop.bind(this);
+		this.handleDragStart = this.handleDragStart.bind(this);
+		this.handleDrag = this.handleDrag.bind(this);
+	}
 
-  handleDragStop(event, ui) {
-    this.props.onNodeStop(this.props.nid, ui.position);
-  }
+	handleDragStart(event, ui) {
+		this.props.onNodeStart(this.props.nid, ui);
+	}
 
-  handleDrag(event, ui) {
-    this.props.onNodeMove(this.props.index, ui.position);
-  }
+	handleDragStop(event, ui) {
+		this.props.onNodeStop(this.props.nid, ui.position);
+	}
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.selected !== nextState.selected;
-  }
+	handleDrag(event, ui) {
+		this.props.onNodeMove(this.props.index, ui.position);
+	}
 
-  onStartConnector(index) {
-    this.props.onStartConnector(this.props.nid, index);
-  }
+	shouldComponentUpdate(nextProps, nextState) {
+		return this.state.selected !== nextState.selected;
+	}
 
-  onCompleteConnector(index) {
-    this.props.onCompleteConnector(this.props.nid, index);
-  }
+	onStartConnector(index) {
+		this.props.onStartConnector(this.props.nid, index);
+	}
 
-  handleClick(e) {
-    this.setState({selected: true});
-    if (this.props.onNodeSelect) {
-      this.props.onNodeSelect(this.props.nid);
-    }
-  }
+	onCompleteConnector(index) {
+		this.props.onCompleteConnector(this.props.nid, index);
+	}
 
-  handleClickOutside() {
-    let {selected} = this.state;
-    if (this.props.onNodeDeselect && selected) {
-      this.props.onNodeDeselect(this.props.nid);
-    }
-    this.setState({selected: false});
-  }
+	handleClick(e) {
+		this.setState({selected: true});
+		if (this.props.onNodeSelect) {
+			this.props.onNodeSelect(this.props.nid);
+		}
+	}
+
+	handleClickOutside() {
+		let {selected} = this.state;
+		if (this.props.onNodeDeselect && selected) {
+			this.props.onNodeDeselect(this.props.nid);
+		}
+		this.setState({selected: false});
+	}
 
 	render() {
-    let {selected} = this.state;
+		let {selected} = this.state;
 
-    let nodeClass = 'node' + (selected ? ' selected' : '');
+		let nodeClass = 'node' + (selected ? ' selected' : '');
 
+		const payload = {
+			pos: this.props.pos,
+			selected,
+			color: this.props.color,
+			title: this.props.title,
+			inputs: this.props.inputs,
+			outputs: this.props.outputs,
+			onStartConnector: this.onStartConnector,
+			handleDragStart: this.handleDragStart,
+			handleDragStop: this.handleDragStop,
+			handleDrag: this.handleDrag,
+			onCompleteConnector: this.onCompleteConnector
+		};
 		return (
-		  <div onDoubleClick={(e) => {this.handleClick(e)}}>
-        <Draggable
-          start={{x:this.props.pos.x,y:this.props.pos.y}}
-          handle=".node-header"
-          onStart={(event, ui)=>this.handleDragStart(event, ui)}
-          onStop={(event, ui)=>this.handleDragStop(event, ui)}
-          onDrag={(event, ui)=>this.handleDrag(event, ui)}>
-        <section className={nodeClass} style={{zIndex:10000}}>
-            <header className="node-header" style={{backgroundColor:this.props.color}}>
-              <span className="node-title">{this.props.title}</span>
-            </header>
-            <div className="node-content">
-              <NodeInputList items={this.props.inputs} onCompleteConnector={(index)=>this.onCompleteConnector(index)} />
-              <NodeOuputList items={this.props.outputs} onStartConnector={(index)=>this.onStartConnector(index)} />
-            </div>
-        </section>
-        </Draggable>
-      </div>
-    );
+			<div onDoubleClick={(e) => {
+				this.handleClick(e)
+			}}>
+				<QuestText {...payload}/>
+				{/*<Draggable*/}
+				{/*start={{x: this.props.pos.x, y: this.props.pos.y}}*/}
+				{/*handle=".node-header"*/}
+				{/*onStart={(event, ui) => this.handleDragStart(event, ui)}*/}
+				{/*onStop={(event, ui) => this.handleDragStop(event, ui)}*/}
+				{/*onDrag={(event, ui) => this.handleDrag(event, ui)}>*/}
+				{/*<section className={nodeClass} style={{zIndex: 10000}}>*/}
+				{/*<header className="node-header" style={{backgroundColor: this.props.color}}>*/}
+				{/*<span className="node-title">{this.props.title}</span>*/}
+				{/*</header>*/}
+				{/*<div className="node-content">*/}
+				{/*<NodeInputList items={this.props.inputs}*/}
+				{/*onCompleteConnector={(index) => this.onCompleteConnector(index)}/>*/}
+				{/*<NodeOuputList items={this.props.outputs}*/}
+				{/*onStartConnector={(index) => this.onStartConnector(index)}/>*/}
+				{/*</div>*/}
+				{/*</section>*/}
+				{/*</Draggable>*/}
+			</div>
+		);
 	}
 }
 
---
+++ src/lib/QuestText.react.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import NodeInputList from './NodeInputList';
+import NodeOutputList from './NodeOutputList';
+var Draggable = require('react-draggable');
+
+export default function QuestText(props) {
+	const {selected, color, title, inputs, outputs, pos,} = props;
+	const {onStartConnector, onCompleteConnector, handleDragStart, handleDragStop, handleDrag} = props;
+	const nodeClass = 'node' + (selected ? ' selected' : '');
+	return (
+		<Draggable
+			start={{x: pos.x, y: pos.y}}
+			handle=".node-header"
+			onStart={(event, ui) => handleDragStart(event, ui)}
+			onStop={(event, ui) => handleDragStop(event, ui)}
+			onDrag={(event, ui) => handleDrag(event, ui)}>
+
+			<section className={nodeClass} style={{zIndex: 10000}}>
+				<header className="node-header" style={{backgroundColor: color}}>
+					<span className="quest-text-header">{title}</span>
+				</header>
+				<div className="node-content">
+					<NodeInputList items={inputs}
+					               onCompleteConnector={(index) => onCompleteConnector(index)}/>
+					<NodeOutputList items={outputs} onStartConnector={(index) => onStartConnector(index)}/>
+				</div>
+			</section>
+		</Draggable>
+
+	)
+}
+
