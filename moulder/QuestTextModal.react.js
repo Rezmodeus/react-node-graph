edd6f781cf710b27abaa27c97278c96bdbca54ba
Rezmodeus <rezmodeus@yahoo.com>
2017-08-15 23:25:23 +0200

    modal form wip

--- moulder/QuestTextModal.react.js
+++ moulder/QuestTextModal.react.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Actions from './Actions';
-import {Modal, ModalHeader, ModalTitle, ModalBody,} from 'react-bootstrap';
+import {Modal, Button, Form, FormControl, FormGroup, FieldGroup, ControlLabel, Col} from 'react-bootstrap';
 
 
 export class QuestTextModal extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {...this.props.nodeObj};
+		this.onChange = this.onChange.bind(this);
+	}
+
+	doSubmit(e) {
+		e.preventDefault();
+	}
+
+	onChange(prop, e) {
+		this.setState({[prop]: e.target.value});
 	}
 
 	render() {
@@ -16,13 +25,35 @@ export class QuestTextModal extends Component {
 			<Modal className="edit-modal" show={true}
 			       onHide={() => this.props.hideModal()}>
 				<Modal.Header>
-					<Modal.Title>Edit node</Modal.Title>
+					<Modal.Title>Edit node {this.props.selectedNode}</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<h4>General</h4>
-					{this.props.selectedNode}
-					<h4>Permissions</h4>
+					<form>
+						<h4>Text</h4>
+						<FormGroup controlId="formControlsTextarea">
+							<ControlLabel>Textarea</ControlLabel>
+							<FormControl componentClass="textarea" placeholder="textarea"/>
+						</FormGroup>
+						<h4>Choices</h4>
+						<FormGroup controlId="formInlineName">
+							<Col sm={10}>
+								<FormControl type="text" placeholder="Choice"
+								             onChange={(event) => this.onChange('text', event)}
+								             value={'hej'}
+								/>
+							</Col>
+							<Col sm={2}>
+								<Button bsStyle="primary" bsSize="small" title="something"
+								        onClick={() => console.log('button press')}>something</Button>
+							</Col>
+						</FormGroup>
+
+					</form>
 				</Modal.Body>
+				<Modal.Footer>
+					<h4>Footer</h4>
+				</Modal.Footer>
+
 			</Modal>
 		);
 	}
@@ -30,7 +61,8 @@ export class QuestTextModal extends Component {
 
 function mapStateToProps(state) {
 	return {
-		selectedNode: state.selectedNode
+		selectedNode: state.selectedNode,
+		nodeObj: state.graph.nodes[state.selectedNode]
 	};
 }
 
