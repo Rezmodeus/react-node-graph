40179500f1c79729b378c0eba9be0f9514224fd6
Rezmodeus <rezmodeus@yahoo.com>
2017-09-22 16:40:11 +0200

    nodeView content from node displayed

--- moulder/player/NodeView.react.js
+++ moulder/player/NodeView.react.js
@@ -1,20 +1,39 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Actions from '../Actions';
-import {Well} from 'react-bootstrap';
+import {Well, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 export class NodeView extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {
-		};
+		this.state = {};
+	}
+
+	onClick(index) {
+		console.log('click',index);
+
 	}
 
 	render() {
+		const node = this.props.nodes.find(n =>n.nid === this.props.questPlayerCurrentNid);
+		if(!node) {
+			return (<Well>no node with nid:{this.props.questPlayerCurrentNid}</Well>);
+		}
+		if(node.contentType !== 'questText'){
+			return (<Well>no questText node, nid:{this.props.questPlayerCurrentNid}</Well>);
+		}
+		const choices = node.fields.out.map((out,index) => <ListGroupItem key={out.str} onClick={() => this.onClick(index)}>{out.str}</ListGroupItem>);
 		return (
 			<div>
-				<Well>NodeView</Well>
+				<Well>
+					<h3>{node.type}</h3>
+					<ListGroup>
+						{choices}
+					</ListGroup>
+
+
+				</Well>
 			</div>
 		);
 	}
@@ -22,12 +41,13 @@ export class NodeView extends Component {
 
 function mapStateToProps(state) {
 	return {
+		nodes: state.graph.nodes,
+		questPlayerCurrentNid: state.questPlayerCurrentNid
 	};
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-	};
+	return {};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(NodeView);
